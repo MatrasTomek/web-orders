446119759081b9b41f85c7619ed976bc5e12da46
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-02-14 21:33:18 +0100

    Add login page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [LoginPageComponent],
-  imports: [CommonModule, SharedModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+  ],
   exports: [LoginPageComponent],
 })
 export class PagesModule {}
