e2f0e4d483fe682d5e92e219717b98c46eb5d4f8
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-04-22 21:20:27 +0200

    Add global filter to customer table

--- src/app/pages/customers-page/customers-page.component.ts
+++ src/app/pages/customers-page/customers-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import { CustomerService } from 'src/app/services/customer.service';
 import { Table } from 'primeng/table';
@@ -9,19 +9,27 @@ import { Table } from 'primeng/table';
   styleUrls: ['./customers-page.component.scss'],
 })
 export class CustomersPageComponent {
+  @ViewChild('customersTb') customersTable!: Table;
+
   constructor(public modal: ModalService, public customers: CustomerService) {
     this.getUsers();
   }
 
-  allUsers: any;
+  allUsers: [] = [];
 
   async getUsers() {
     this.allUsers = await this.customers.getCustomers();
-    console.log(this.allUsers);
   }
   openModal($event: Event) {
     $event.preventDefault();
     this.modal.toggleModal('customer');
+  }
+
+  handleInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement && inputElement.value != null) {
+      this.customersTable.filterGlobal(inputElement.value, 'contains');
+    }
   }
 
   clear(table: Table) {
