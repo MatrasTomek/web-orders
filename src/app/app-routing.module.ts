5ff1cf29079227ba6d8688d0486d2f7bd39a515c
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-05-21 20:37:46 +0200

    Clear unused components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartPageComponent } from './pages/start-page/start-page.component';
 import { OrdersPageComponent } from './pages/orders-page/orders-page.component';
-import { NewOrderPageComponent } from './pages/new-order-page/new-order-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { CustomersPageComponent } from './pages/customers-page/customers-page.component';
-import { AddCustomerComponent } from './pages/add-customer/add-customer.component';
 import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
@@ -21,26 +19,13 @@ const routes: Routes = [
     },
   },
   {
-    path: 'new-order',
-    component: NewOrderPageComponent,
-    data: {
-      isAuth: true,
-    },
-  },
-  {
     path: 'customers',
     component: CustomersPageComponent,
     data: {
       isAuth: true,
     },
   },
-  {
-    path: 'add-customer',
-    component: AddCustomerComponent,
-    data: {
-      isAuth: true,
-    },
-  },
+
   {
     path: 'about',
     component: AboutComponent,
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,10 +7,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { StartPageComponent } from './start-page/start-page.component';
 import { OrdersPageComponent } from './orders-page/orders-page.component';
-import { NewOrderPageComponent } from './new-order-page/new-order-page.component';
+
 import { PagesRoutingModule } from './pages-routing.module';
 import { CustomersPageComponent } from './customers-page/customers-page.component';
-import { AddCustomerComponent } from './add-customer/add-customer.component';
 import { AboutComponent } from './about/about.component';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
@@ -23,9 +22,7 @@ import { ConfirmationModalModule } from '../confirmation-modal/confirmation-moda
     LoginPageComponent,
     StartPageComponent,
     OrdersPageComponent,
-    NewOrderPageComponent,
     CustomersPageComponent,
-    AddCustomerComponent,
     AboutComponent,
   ],
   imports: [
