8cacdb3e4e64ef2263608db06c42e06e50c9ece6
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-10-04 12:48:08 +0200

    Fix pdf print, use print to canvas

--- src/app/show-order-modal/show-order-modal.component.ts
+++ src/app/show-order-modal/show-order-modal.component.ts
@@ -2,6 +2,9 @@ import { Component, Input } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { IOrder } from '../models/order.model';
 import jsPDF from 'jspdf';
+import  utf8encode  from 'jspdf';
+import html2canvas from 'html2canvas';
+import { customFontBase64} from '../../assets/custom-font'
 
 @Component({
   selector: 'app-show-order-modal',
@@ -44,37 +47,54 @@ public convertedUnloadDate: Date | null = null
   }
 
   generatePDF(orderNumber: any) {
-
     const doc = new jsPDF('p', 'mm', 'a4');
+
+
+
     const htmlContent  = document.getElementById('htmlData');
-
-    console.log(orderNumber, htmlContent);
-
 
     if (htmlContent ) {
 
-      const htmlWidth = htmlContent.offsetWidth;
-      const htmlHeight = htmlContent.offsetHeight;
+      // const htmlWidth = htmlContent.offsetWidth;
+      // const htmlHeight = htmlContent.offsetHeight;
 
 
-      const pageWidth = doc.internal.pageSize.getWidth();
-      const pageHeight = doc.internal.pageSize.getHeight();
+      // const pageWidth = doc.internal.pageSize.getWidth();
+      // const pageHeight = doc.internal.pageSize.getHeight();
 
 
-      const scale = Math.min(pageWidth / htmlWidth, pageHeight / htmlHeight) * 0.9;
+      // const scale = Math.min(pageWidth / htmlWidth, pageHeight / htmlHeight) * 0.9;
+
+
 
       setTimeout(()=>{
 
-        doc.html(htmlContent, {
-          callback: function (doc) {
+        html2canvas(htmlContent).then(canvas => {
 
-            doc.save(`zlecenie_${orderNumber}_.pdf`);
-          },
+          const imgData = canvas.toDataURL('image/png');
+          const a4Width = 210;
+          const a4Height = 297;
+          const imgWidth = 190;
+          let imgHeight = (canvas.height * imgWidth) / canvas.width;
 
-          x: 10,
-          y: 10,
-          width: 170,
-          html2canvas: {scale: scale},
+          const positionX = (a4Width - imgWidth) / 2;
+          let positionY = 10;
+
+          while (imgHeight > 0) {
+            if (imgHeight > a4Height - positionY) {
+              doc.addImage(imgData, 'PNG', positionX, positionY, imgWidth, a4Height - positionY);
+              doc.addPage();
+              imgHeight -= (a4Height - positionY);
+              positionY = 0;
+            } else {
+
+              doc.addImage(imgData, 'PNG', positionX, positionY, imgWidth, imgHeight);
+              imgHeight = 0;
+            }
+          }
+
+
+          doc.save(`zlecenie_${orderNumber}_.pdf`);
         });
       }, 500)
     }
