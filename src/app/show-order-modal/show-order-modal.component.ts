8651987a4ad3277ed7c813bd288139b1c659f14a
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-11-01 18:58:58 +0100

    Fix pdf a4 generator on height

--- src/app/show-order-modal/show-order-modal.component.ts
+++ src/app/show-order-modal/show-order-modal.component.ts
@@ -33,40 +33,34 @@ export class ShowOrderModalComponent {
 
 		const htmlContent = document.getElementById('htmlData');
 
-		if (this.activeOrder) {
-			const { orderDetails, carrierDetails } = this.activeOrder;
-			this.loadAdress = orderDetails?.loadAddress.replace(/[0-9 -]/g, '').replace(/ /g, '_');
-			this.unloadAdress = orderDetails?.unloadAddress.replace(/[0-9 -]/g, '').replace(/ /g, '_');
-			this.customerName =
-				carrierDetails?.name.replace(/ /g, '').length > 15
-					? carrierDetails?.name.replace(/ /g, '').slice(0, 15)
-					: carrierDetails?.name.replace(/ /g, '');
-		}
+		// if (this.activeOrder) {
+		// 	const { orderDetails, carrierDetails } = this.activeOrder;
+		// 	this.loadAdress = orderDetails?.loadAddress.replace(/[0-9 -]/g, '').replace(/ /g, '_');
+		// 	this.unloadAdress = orderDetails?.unloadAddress.replace(/[0-9 -]/g, '').replace(/ /g, '_');
+		// 	this.customerName =
+		// 		carrierDetails?.name.replace(/ /g, '').length > 15
+		// 			? carrierDetails?.name.replace(/ /g, '').slice(0, 15)
+		// 			: carrierDetails?.name.replace(/ /g, '');
+		// }
 
 		if (htmlContent) {
 			this.store.dispatch(showSpinner());
 			setTimeout(() => {
-				html2canvas(htmlContent).then((canvas) => {
+				html2canvas(htmlContent, {
+					scale: 2,
+					useCORS: true,
+					backgroundColor: '#fff',
+				}).then((canvas) => {
 					const imgData = canvas.toDataURL('image/png');
-					const a4Width = 210;
-					const a4Height = 250;
-					const imgWidth = 190;
-					let imgHeight = (canvas.height * imgWidth) / canvas.width - 35;
+
+					const a4Width = doc.internal.pageSize.getWidth();
+					const a4Height = doc.internal.pageSize.getHeight();
+
+					const imgWidth = a4Width - 20;
+					const imgHeight = ((canvas.height * imgWidth) / canvas.width) * 0.85;
 
 					const positionX = (a4Width - imgWidth) / 2;
-					let positionY = 10;
-
-					// while (imgHeight > 0) {
-					// 	if (imgHeight > a4Height - positionY) {
-					// 		doc.addImage(imgData, 'PNG', positionX, positionY, imgWidth, a4Height - positionY);
-					// 		doc.addPage();
-					// 		imgHeight -= a4Height - positionY;
-					// 		positionY = 0;
-					// 	} else {
-					// 		doc.addImage(imgData, 'PNG', positionX, positionY, imgWidth, imgHeight);
-					// 		imgHeight = 0;
-					// 	}
-					// }
+					const positionY = 5;
 
 					doc.addImage(imgData, 'PNG', positionX, positionY, imgWidth, imgHeight);
 
